335245ed7ed4e4ac88c2a6b95f91e45ea29b58b7
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-11-24 14:24:56 +0100

    update tasks-baseTypes

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,23 +1,35 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+type DataPage = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: DateDetails;
+};
 
-const page1 = {
-  title: 'The awesome page',
+type DateDetails = {
+  createAt: Date;
+  updateAt: Date;
+};
+
+const page1: DataPage = {
+  title: "The awesome page",
   likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
   details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
 
-const page2 = {
-  title: 'Python or Js',
+const page2: DataPage = {
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
